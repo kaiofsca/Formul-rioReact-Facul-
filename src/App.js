30ab672aa6015273518d6e1f1bffad3334946564
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-10-28 17:17:07 -0300

    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import './global.css'
+import './styles.css'
+
+const verificacao = yup.object({
+	firstName: yup.string().required("Primeiro nome é obrigatório!").max(10, "Máximo de 10 caracteres").min(3, "Mínimo de 3 caracter"), // Dizendo que é uma string e que é obrigatório
+	lastName: yup.string().required("Sobre nome é obrigatório!").max(10, "Máximo de 10 caracteres").min(3, "Mínimo de 3 caracter"),
+	job: yup.string().required("Profissão é obrigatório!").max(10, "Máximo de 10 caracteres").min(3, "Mínimo de 3 caracter"),
+	gender: yup.string().required("Gênero é obrigatório!"),
+	age: yup.string().required("Idade é obrigatório!"),
+}).required();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Desestruturando esses elementos de dentro da biblioteca	
+	const { register, handleSubmit, formState: { errors } } = useForm({
+		resolver: yupResolver(verificacao)
+	})
+
+	const onSubmitForm = data => console.log(data)
+
+	return (
+		<div className="app">
+			<form onSubmit={handleSubmit(onSubmitForm)}> {/* sinaliza que estamos enviando uma função dps que faz o submit */}
+				<h1>Formulário Brabo</h1>
+
+				<div className='field' >
+					<label className='label'>Nome</label>
+					<input type="text" {...register("firstName")}/> {/* (...) desconstroi o register */}
+					<p>{errors.firstName?.message}</p>
+				</div>
+
+				<div className='field' >
+					<label className='label'>Sobre nome</label>
+					<input type="text" {...register("lastName")}/>
+					<p>{errors.lastName?.message}</p>
+				</div>
+
+				<div className='field' >
+					<label className='label'>Profissão</label>
+					<input type="text" {...register("job")}/> 
+					<p>{errors.job?.message}</p>
+				</div>
+
+				<div className='box'>
+					<div className='field fieldAlternative'>
+						<label className='label'>Gênero</label>
+						<select {...register("gender")}>
+							<option value="">Selecione</option>
+							<option value="female">Feminino</option>
+							<option value="male">Masculino</option>
+							<option value="other">Outro</option>
+						</select>
+						<p>{errors.gender?.message}</p>
+					</div>
+
+					<div className='field fieldAlternative' >
+						<label>Idade</label>
+						<input type="number" {...register("age", { min: 18, max: 99 })}/>
+						<p>{errors.age?.message}</p>
+					</div>
+				</div>
+
+				<button type='submit'>CADASTRAR</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
