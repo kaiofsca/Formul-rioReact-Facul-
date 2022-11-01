c4840eb8ec81623745232d0644ff532ad6daaa02
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-10-31 21:35:12 -0300

    add rotas e paginas novas

--- src/App.js
+++ src/App.js
@@ -1,73 +1,21 @@
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './global.css'
-import './styles.css'
+import Form from './pages/Form';
+import Home from './pages/Home';
+import End from './pages/End';
 
-const verificacao = yup.object({
-	firstName: yup.string().required("Primeiro nome é obrigatório!").max(10, "Máximo de 10 caracteres").min(3, "Mínimo de 3 caracter"), // Dizendo que é uma string e que é obrigatório
-	lastName: yup.string().required("Sobre nome é obrigatório!").max(10, "Máximo de 10 caracteres").min(3, "Mínimo de 3 caracter"),
-	job: yup.string().required("Profissão é obrigatório!").max(30, "Máximo de 30 caracteres").min(3, "Mínimo de 3 caracter"),
-	gender: yup.string().required("Gênero é obrigatório!"),
-	age: yup.string().required("Idade é obrigatório!"),
-}).required();
+import './styles/global.css'
 
 function App() {
-	// Desestruturando esses elementos de dentro da biblioteca	
-	const { register, handleSubmit, formState: { errors } } = useForm({
-		resolver: yupResolver(verificacao)
-	})
-
-	const onSubmitForm = data => {
-		console.log(data)
-		alert('Cadastro realizado com sucesso!')
-	}
-
 	return (
 		<div className="app">
-			<form onSubmit={handleSubmit(onSubmitForm)}> {/* sinaliza que estamos enviando uma função dps que faz o submit */}
-				<h1>Formulário Brabo</h1>
-
-				<div className='field' >
-					<label className='label'>Nome</label>
-					<input type="text" {...register("firstName")}/> {/* (...) desconstroi o register */}
-					<p>{errors.firstName?.message}</p>
-				</div>
-
-				<div className='field' >
-					<label className='label'>Sobre nome</label>
-					<input type="text" {...register("lastName")}/>
-					<p>{errors.lastName?.message}</p>
-				</div>
-
-				<div className='field' >
-					<label className='label'>Profissão</label>
-					<input type="text" {...register("job")}/> 
-					<p>{errors.job?.message}</p>
-				</div>
-
-				<div className='box'>
-					<div className='field fieldAlternative'>
-						<label className='label'>Gênero</label>
-						<select {...register("gender")}>
-							<option value="">Selecione</option>
-							<option value="female">Feminino</option>
-							<option value="male">Masculino</option>
-							<option value="other">Outro</option>
-						</select>
-						<p>{errors.gender?.message}</p>
-					</div>
-
-					<div className='field fieldAlternative' >
-						<label>Idade</label>
-						<input type="number" {...register("age", { min: 18, max: 99 })}/>
-						<p>{errors.age?.message}</p>
-					</div>
-				</div>
-
-				<button type='submit'>CADASTRAR</button>
-			</form>
+			<BrowserRouter>
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+          			<Route path='/form' element={<Form />} />
+         			<Route path='/end' element={<End />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
---
+++ src/pages/End/index.js
@@ -0,0 +1,13 @@
+
+
+import './end.css'
+
+function End() {
+	return (
+		<div className="app">
+			<h1>end</h1>
+		</div>
+	);
+}
+
+export default End;
